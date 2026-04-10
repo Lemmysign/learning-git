91aa98771ed6c1088f6d984413ee4c8df4ff45e6
Lemuel Inneh <148467555+Lemmysign@users.noreply.github.com>
2026-04-11 00:23:34 +0100

    added full JS code

---
+++ main.js
@@ -0,0 +1,14 @@
+// Print to console
+console.log("Hello World 👋");
+
+// Function to show a message
+function showMessage() {
+    alert("Welcome to JavaScript 🚀");
+}
+
+// Simple calculation
+let a = 10;
+let b = 5;
+let sum = a + b;
+
+console.log("Sum is: " + sum);
